0b8b1394b20f93d434df010eb7757789ef4a3558
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2014-12-08 18:02:39 +0100

    Declaración y definición de Celdas y Nodos de un grafo de escena

--- inc/grafo_escena.hpp
+++ inc/grafo_escena.hpp
@@ -1,4 +1,79 @@
 #ifndef IG_GRAFO_ESCENA_HPP
 #define IG_GRAFO_ESCENA_HPP
 
+#include <vector>
+#include "Matriz.hpp"
+#include "Malla_TVT.hpp"
+
+//--------------------INTERFAZ-----------------------//
+
+class Celda_NGE{
+public:
+    virtual ~Celda_NGE() {};
+    virtual void procesar(){};
+};
+
+//----------------------NODO-------------------------//
+
+typedef std::vector<Celda_NGE> lista_celdas;
+
+class Nodo{
+private:
+    lista_celdas escena;
+
+public:
+    void visualizar();
+
+    const Celda_NGE& operator[](unsigned int i) const{ return this->escena[i]; };
+    Celda_NGE& operator[](unsigned int i){ return this->escena[i]; };
+
+    void push_back(Celda_NGE celda){ escena.push_back(celda); };
+    void pop_back(){ escena.pop_back(); };
+};
+
+//---------------------HIJOS-------------------------//
+
+class Celda_Nodo : public Celda_NGE{
+private:
+    Nodo hijo;
+
+public:
+    Celda_Nodo(Nodo hijo_);
+    virtual void procesar();
+};
+
+//---------------------------------------------------//
+
+class Celda_Transformacion : public Celda_NGE{
+private:
+    Matriz4x4f matriz;
+
+public:
+    Celda_Transformacion(Matriz4x4f matriz_);
+    virtual void procesar();
+};
+
+//-----------------OBJETOS SIMPLES--------------------//
+
+class Celda_Malla : public Celda_NGE{
+private:
+    Malla_TVT malla;
+
+public:
+    Celda_Malla(Malla_TVT malla);
+    virtual void procesar();
+};
+
+//---------------------------------------------------//
+
+class Celda_SolidoGLUT : public Celda_NGE{
+private:
+    enum solidos_GLUT solido;
+
+public:
+    ~Celda_SolidoGLUT(){}
+    Celda_SolidoGLUT(enum solidos_GLUT solido);
+    virtual void procesar();
+};
+
 #endif
--- src/grafo_escena.cpp
+++ src/grafo_escena.cpp
@@ -1,1 +1,88 @@
 #include "grafo_escena.hpp"
+
+//---------------------HIJOS-------------------------//
+
+Celda_Nodo::Celda_Nodo(Nodo hijo_){
+    this->hijo = hijo_;
+}
+
+void Celda_Nodo::procesar(){
+    hijo.visualizar();
+}
+
+//---------------------------------------------------//
+
+Celda_Transformacion::Celda_Transformacion(Matriz4x4f matriz_){
+    this->matriz = matriz_;
+}
+
+void Celda_Transformacion::procesar(){
+    glMultMatrixf(this->matriz.data());
+}
+
+//---------------------------------------------------//
+
+Celda_Malla::Celda_Malla(Malla_TVT malla_){
+    this->malla = malla_;
+}
+
+void Celda_Malla::procesar(){
+    malla.DibujarMalla_TVT();
+}
+
+//---------------------------------------------------//
+
+Celda_SolidoGLUT::Celda_SolidoGLUT(enum solidos_GLUT solido_){
+    this->solido = solido_;
+}
+
+void Celda_SolidoGLUT::procesar(){
+    switch( this->solido ){
+        case ESFERA:
+            glutSolidSphere(1.0, 100, 100);
+            break;
+
+        case CUBO:
+            glutSolidCube(1.0);
+            break;
+
+        case CONO:
+            glutSolidCone(1.0, 2.0, 100, 100);
+            break;
+
+        case TORO:
+            glutSolidTorus(1.0, 2.0, 100, 100);
+            break;
+
+        case DODECAEDRO:
+            glutSolidDodecahedron();
+            break;
+
+        case OCTAEDRO:
+            glutSolidOctahedron();
+            break;
+
+        case TETRAEDRO:
+            glutSolidTetrahedron();
+            break;
+
+        case ICOSAEDRO:
+            glutSolidIcosahedron();
+            break;
+
+        case TETERA:
+            glutSolidTeapot(1.0);
+            break;
+    }
+}
+
+//---------------------------------------------------//
+
+void Nodo::visualizar(){
+    for(size_t i = 0; i < this->escena.size(); i++)
+    {
+        glPushMatrix();
+        escena[i].procesar();
+        glPopMatrix();
+    }
+}
