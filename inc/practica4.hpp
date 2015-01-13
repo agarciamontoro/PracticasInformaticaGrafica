6eb7e13032b75839a078cd29c90563fc6ad71848
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-01-13 23:44:42 +0100

    Nueva práctica

---
+++ inc/practica4.hpp
@@ -0,0 +1,40 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2014-15
+// **
+// **
+// ** Práctica 4  (declaraciones públicas)
+// **
+// *********************************************************************
+
+#ifndef IG_PRACTICA4_HPP
+#define IG_PRACTICA4_HPP
+
+#include <iostream>
+#include <vector>
+#include <assert.h>
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+#include "tuplas.hpp"
+#include "error-ogl.hpp"
+#include "file_ply_stl.hpp"
+#include "VBO.hpp"
+#include "Malla_TVT.hpp"
+#include "tipos.hpp"
+
+void P4_DibujarObjetos() ;
+void P4_Inicializar( int argc, char *argv[] ) ;
+void P4_CambiarVisualizacion(enum modo_visualizacion modo);
+void P4_Conmutar_NormalesCaras();
+void P4_Conmutar_NormalesVertices();
+
+void P4_Modificar_Grado_Libertad(enum grados_libertad grado, float cambio);
+void P4_Modificar_Velocidad_Animacion(double incremento);
+void P4_Establecer_Valores_Animacion();
+
+bool P4_FGE_TeclaNormal( unsigned char tecla, int x_raton, int y_raton );
+bool P4_FGE_TeclaEspecial( int tecla, int x_raton, int y_raton );
+
+#endif
---
+++ src/practica4.cpp
@@ -0,0 +1,56 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2014-15
+// **
+// ** Práctica 4 (implementación)
+// **
+// *********************************************************************
+
+#include "practica4.hpp"
+#include "grafo_escena.hpp"
+
+#include <stdio.h>
+
+// ---------------------------------------------------------------------
+//  Cambia el modo de visualización del modelo PLY
+
+void P4_CambiarVisualizacion(enum modo_visualizacion modo){
+}
+
+// ---------------------------------------------------------------------
+// Función para implementar en la práctica 2 para inicialización.
+// Se llama una vez al inicio, cuando ya se ha creado la ventana e
+// incializado OpenGL. El PLY se debe cargar aquí.
+
+void P4_Inicializar( int argc, char *argv[] ){
+
+}
+
+// ---------------------------------------------------------------------
+// Función a implementar en la práctica 2  para dibujar los objetos
+
+void P4_DibujarObjetos(){
+}
+
+void P4_Conmutar_NormalesCaras(){
+}
+
+void P4_Conmutar_NormalesVertices(){
+}
+
+void P4_Modificar_Grado_Libertad(enum grados_libertad grado, float cambio){
+}
+
+void P4_Modificar_Velocidad_Animacion(double incremento){
+}
+
+void P4_Establecer_Valores_Animacion(){
+}
+
+bool P4_FGE_TeclaNormal( unsigned char tecla, int x_raton, int y_raton ){
+    return false;
+}
+
+bool P4_FGE_TeclaEspecial( int tecla, int x_raton, int y_raton ){
+    return false;
+}
